eb1e30f36a497071a44550c03ce80e561191d395
soliloquy-ray <santos.ray.rommel@gmail.com>
2018-08-01 10:26:00 +0800

    add forgot password handler

--- src/pages/forgot-password/forgot-password.ts
+++ src/pages/forgot-password/forgot-password.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 import { LoginPage } from '../login/login';
 
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+import { config } from '../../ext/config';
 /**
  * Generated class for the ForgotPasswordPage page.
  *
@@ -19,7 +22,9 @@ declare var mobilecheck; //fn to check for screen type
 export class ForgotPasswordPage {
 	email: string;
 	isMobile : boolean = mobilecheck();
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  env = config[location.origin].backend;
+  ls = localStorage.lastSent;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, private loader: LoadingController) {
   }
 
   ionViewDidLoad() {
@@ -27,7 +32,38 @@ export class ForgotPasswordPage {
   }
 
   forgotten(){
+    let load = this.loader.create({
+      spinner: 'crescent',
+      showBackdrop: true,
+      content: `Processing...`,
+      dismissOnPageChange: true
+    });
+    load.present();
 
+    let hdr = new Headers;
+    hdr.append('Content-Type','application/json');
+    let rq = new RequestOptions;
+    rq.headers = hdr;
+    
+    this.http.post(`${this.env}/api.php?q=forgotpassmail`,{email:this.email}, rq)
+              .toPromise()
+              .then(res=>{
+                load.dismiss();
+                if(res.status == 200){
+                  this.ls = Date.now();
+                  localStorage.lastSent = this.ls;
+                }else{
+                  console.warn(res.statusText);
+                }
+              })
+              .catch(err=>{
+                load.dismiss();
+                console.warn(err);
+              });
+  }
+
+  lastSent():boolean{
+    return (Date.now() >= parseInt(this.ls)+60000);
   }
 
   toHome(){
